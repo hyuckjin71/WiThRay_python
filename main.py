319ba533b4356caf9e9f010f9fdb694300b426e0
hyuckjin71 <138177543+hyuckjin71@users.noreply.github.com>
2025-01-10 16:10:17 -0800

    250110_2
    Complete the coding "next_nodes"

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ def print_mouse_coordinates(evt):
         print(f"Mouse Over Coordinates: {evt.picked3d}")
 
 # Select the device for acceleration
-if torch.cuda.is_available():
-    device = torch.device("cuda")
-    print("Using CUDA")
-elif torch.backends.mps.is_available():
-    device = torch.device("mps")
-    print("Using MPS")
-else:
-    device = torch.device("cpu")
-    print("Using CPU")
+# if torch.cuda.is_available():
+#     device = torch.device("cuda")
+#     print("Using CUDA")
+# elif torch.backends.mps.is_available():
+#     device = torch.device("mps")
+#     print("Using MPS")
+# else:
+device = torch.device("cpu")
+print("Using CPU")
 
 mesh = MESH("Manhattan",
             device,
@@ -29,7 +29,7 @@ mesh = MESH("Manhattan",
 # # mesh.mesh_file.alpha(0.1)
 # # msg = Text3D('Tx', pos=[25.4229, -167.19, -1])
 #
-# plotter = Plotter(title="Mouse Over Coordinates Example", axes=1)
+# plotter = Plotter(title="Mouse Over Coordinates Exampxle", axes=1)
 # plotter.add_callback("mouse move", print_mouse_coordinates)
 # plotter.show(mesh.mesh_file, p1, l1).interactive()
 
--- withray/rt/rays.py
+++ withray/rt/rays.py
@@ -53,7 +53,10 @@ class RAYS:
             node_prev = torch.cat([torch.zeros(3,2).to(device=pnts_tx.device), pnts_tx[:,i].view(3,1)], dim=1).view(3,3,1,1)
             info_prev = torch.zeros(1,3, dtype=torch.int32, device=pnts_tx.device).view(1,3,1)
 
+            start_time = time.time()
             cache = self.next_nodes(1, i, node_prev, info_prev, msk_v[i,:].unsqueeze(0))
+            end_time = time.time()
+            print(f"Execution time: {end_time-start_time: .6f} seconds")
 
     def next_nodes(self, level, idx_tx, node_prev, info_prev, msk_v):
 
@@ -73,15 +76,16 @@ class RAYS:
             The next reflective surface must face the previous node, meaning the directional vector 
             should align with the surface's normal vector.
             '''
-            if torch.any(node_prev[:,0:2,-1,i] > 0):
+            case_difrct = torch.any(node_prev[:,0:2,-1,i] > 0)
+            if case_difrct:
                 msk_f[msk_f.clone()] = torch.any(
                     torch.sum( mesh_n * (node_prev[:,0:2,-1,i].view(3,2,1,1) - mesh_v), dim=0) > 0, dim=0
                 )
             else:
                 msk_f[msk_f.clone()] = torch.sum( mesh_n * (node_prev[:,2,-1,i].view(3,1,1,1) - mesh_v), dim=0) > 0
 
-            mesh_v = self.mesh.v[self.mesh.f[msk_f, 0], :].permute(1, 0).view(3, 1, 1,-1)   # (3 x 1 x 1 x num_r) first point of surfaces in msk_f
-            mesh_n = self.mesh.n[:, msk_f].view(3, 1, 1, -1)                                # (3 x 1 x 1 x num_r) normal vector of surfaces in msk_f
+            mesh_v = self.mesh.v[self.mesh.f[msk_f, 0], :].permute(1,0).view(3,1,1,-1)   # (3 x 1 x 1 x num_r) first point of surfaces in msk_f
+            mesh_n = self.mesh.n[:, msk_f].view(3,1,1,-1)                                # (3 x 1 x 1 x num_r) normal vector of surfaces in msk_f
 
             node_r = 2 * torch.sum( (mesh_v - node_prev[:,:,:,i].unsqueeze(-1)) * mesh_n, dim=0).unsqueeze(0) * mesh_n * (node_prev[:,:,:,i] != 0).unsqueeze(-1) + node_prev[:,:,:,i].unsqueeze(-1)
             num_r = node_r.shape[3]
@@ -93,10 +97,70 @@ class RAYS:
                 (torch.cat((idx_tx*torch.ones(1,1,num_r), torch.zeros(1,2,num_r)), dim=1).to(device=self.device),
                  info_prev[1:level,:,i].unsqueeze(-1).repeat(1,1,num_r),
                  torch.cat((torch.where(msk_f)[0].view(1,-1), torch.zeros(1,num_r).to(device=self.device), torch.ones(1,num_r).to(device=self.device)), dim=0).view(1,3,-1)
-                 ), dim=0, dtype = torch.int
-            )                       # ((level+1) x 3 x num_r
+                 ), dim=0
+            ).to(dtype = torch.int)                       # ((level+1) x 3 x num_r
 
             cache.pnts = torch.cat((cache.pnts, pnts), dim=3)
             cache.info = torch.cat((cache.info, info), dim=2)
-            
+
+            msk_d = torch.stack([
+                torch.all( torch.isin(self.mesh.f[:,[0,1]].to(device="cpu"), idc_v), dim=1),
+                torch.all( torch.isin(self.mesh.f[:,[1,2]].to(device="cpu"), idc_v), dim=1),
+                torch.all( torch.isin(self.mesh.f[:,[2,0]].to(device="cpu"), idc_v), dim=1)
+            ], dim=0).to(device=self.device)
+
+            '''
+            Check whether the next vertices form a valid diffracting edge
+            based on whether the previous node (node_prev) was a reflecting or diffracting point.
+
+            A valid diffracting edge can be determined as belonging to the surface that faces the previous node.
+            This is because the diffracting edge may also belong to surfaces that do not face the previous node,
+            but it must belong to at least one surface that faces the previous node.
+            '''
+            for ii in range(3):
+                mesh_v = self.mesh.v[self.mesh.f[msk_d[ii,:], 0], :].permute(1,0).view(3,1,1,-1)
+                mesh_n = self.mesh.n[:, msk_d[ii,:]].view(3,1,1,-1)
+                if case_difrct:
+                    msk_d[ii,msk_d[ii,:].clone()] = torch.any(
+                        torch.sum( mesh_n * (node_prev[:,0:2,-1,i].view(3,2,1,1) - mesh_v), dim=0) >0, dim=0
+                    ) * msk_d[ii,msk_d[ii,:]]
+                else:
+                    msk_d[ii,msk_d[ii,:].clone()] = (
+                            torch.sum( mesh_n * (node_prev[:,2,-1,i].view(3,1,1,1) - mesh_v), dim=0) > 0
+                    ) * msk_d[ii,msk_d[ii,:]]
+
+            idc_d = torch.cat(
+                (torch.where(msk_d[0,:])[0],
+                 torch.where(msk_d[1,:])[0],
+                 torch.where(msk_d[2,:])[0]), dim=0)
+            idc_e = torch.cat(
+                (torch.ones(torch.sum(msk_d[0,:])),
+                 torch.ones(torch.sum(msk_d[1,:]))*2,
+                 torch.ones(torch.sum(msk_d[2,:]))*3), dim=0
+            ).to(device=self.device)
+
+            node_d = torch.cat(
+                (torch.cat((self.mesh.v[self.mesh.f[msk_d[0,:],0],:].permute(1,0).view(3,-1,1),
+                            self.mesh.v[self.mesh.f[msk_d[0,:],1],:].permute(1,0).view(3,-1,1)), dim=2),
+                 torch.cat((self.mesh.v[self.mesh.f[msk_d[1,:],1],:].permute(1,0).view(3,-1,1),
+                            self.mesh.v[self.mesh.f[msk_d[1,:],2],:].permute(1,0).view(3,-1,1)), dim=2),
+                 torch.cat((self.mesh.v[self.mesh.f[msk_d[2,:],2],:].permute(1,0).view(3,-1,1),
+                            self.mesh.v[self.mesh.f[msk_d[2,:],0],:].permute(1,0).view(3,-1,1)), dim=2)), dim=1
+            ).unsqueeze(-1).permute(0,2,3,1)                # (3 x 2 x 1 x num_d) two points of edge in msk_d
+            num_d = node_d.shape[3]
+
+            pnts = torch.cat((node_d, node_prev[:,2,-1,i].view(3,1,1,1).repeat(1,1,1,num_d)), dim=1)
+            pnts = torch.cat((node_prev[:,:,0:level-1,i].unsqueeze(-1).repeat(1,1,1,num_d), pnts),dim=2)
+
+            info = torch.cat(
+                (torch.cat((idx_tx * torch.ones(1, 1, num_d), torch.zeros(1, 2, num_d)), dim=1).to(device=self.device),
+                 info_prev[1:level, :, i].unsqueeze(-1).repeat(1, 1, num_d),
+                 torch.cat((idc_d.view(1,-1), idc_e.view(1,-1), (2*torch.ones(1, num_d)).to(device=self.device)), dim=0).view(1, 3, -1)), dim=0
+            ).to(dtype=torch.int)
+
+            cache.pnts = torch.cat((cache.pnts, pnts), dim=3)
+            cache.info = torch.cat((cache.info, info), dim=2)
+
+            print(f"\r [{100*(i+1)/msk_v.shape[0]: 5.2f} %] {i+1}")
+
             return cache
